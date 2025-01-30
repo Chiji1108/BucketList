ca872190d53edcd8ed40fb40a8d1a8c7e1afcb53
chiji1108 <shingo.pad@gmail.com>
2025-01-30 18:46:33 +0900

    Using Touch ID and Face ID with SwiftUI

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,6 +5,7 @@
 //  Created by 千々岩真吾 on 2025/01/28.
 //
 
+import LocalAuthentication
 import MapKit
 import SwiftUI
 
@@ -24,43 +25,49 @@ let locations = [
 ]
 
 struct ContentView: View {
+    @State private var isUnlocked = false
     @State private var position = MapCameraPosition.region(
         MKCoordinateRegion(
             center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275),
             span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
         )
     )
+
+    func authenticate() {
+        let context = LAContext()
+        var error: NSError?
+
+        // check whether biometric authentication is possible
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            // it's possible, so go ahead and use it
+            let reason = "We need to unlock your data."
+
+            context.evaluatePolicy(
+                .deviceOwnerAuthenticationWithBiometrics, localizedReason: reason
+            ) {
+                success, authenticationError in
+                // authentication has now completed
+                if success {
+                    // authenticated successfully
+                    isUnlocked = true
+                } else {
+                    // there was a problem
+                }
+            }
+        } else {
+            // no biometrics
+        }
+    }
+
     var body: some View {
         VStack {
-            MapReader { proxy in
-                Map()
-                    .onTapGesture { position in
-                        if let coordinate = proxy.convert(position, from: .local) {
-                            print(coordinate)
-                        }
-                    }
-            }
-            HStack(spacing: 50) {
-                Button("Paris") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
-
-                Button("Tokyo") {
-                    position = MapCameraPosition.region(
-                        MKCoordinateRegion(
-                            center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922),
-                            span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-                        )
-                    )
-                }
+            if isUnlocked {
+                Text("Unlocked")
+            } else {
+                Text("Locked")
             }
         }
-
+        .onAppear(perform: authenticate)
     }
 }
 
